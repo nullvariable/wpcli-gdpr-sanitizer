efb2b939851e3f284f5a5050664c27f47c9b5af0
Doug Cone <nullvariable@gmail.com>
2018-06-23 11:06:26 -0400

    add progress bars, start doc blocs, refator actions

--- includes/class-gdpr-sanitizer.php
+++ includes/class-gdpr-sanitizer.php
@@ -1,16 +1,36 @@
 <?php
 /**
- * @package nullvariable\gdpr-sanitizer
+ * @package nullvariable\wpcli-gdpr-sanitizer
  */
 
  class GDPR_Sanitizer extends \WP_CLI_Command {
-     /**
-	 * Holds the command arguments.
+	/**
+	 * User ids to skip.
 	 *
 	 * @var array
 	 */
 	protected $excluded_user_ids = [];
+	/**
+	 * If we can't find a user id for a name or email, should we bail?
+	 *
+	 * @var boolean
+	 */
 	protected $skip_not_found_users = false;
+
+	/**
+     * Performs personal information replacement.
+     *
+     * ## OPTIONS
+     *
+     * [--keep=<user id|user login|email>]
+     * : user(s) to skip during replacment.
+     *
+     * ## EXAMPLES
+     *
+     *     wp gdpr-sanitizer
+	 *     wp gdpr-sanitizer --keep=123
+	 *     wp gdpr-sanitizer --keep="123,admin,test@example.com"
+     */
 	public function __invoke( $args, $assoc_args ) {
 		if ( isset( $assoc_args['keep'] ) && ! empty( $assoc_args['keep'] ) ) {
 			$this->set_excluded_user_ids( $assoc_args['keep'] );
@@ -22,6 +42,11 @@
 		$this->obfuscate_comments();
 	}
 
+	/**
+	 * Rewrite the PII found in standard WordPress comments.
+	 *
+	 * @return void
+	 */
 	protected function obfuscate_comments() {
 		$faker = Faker\Factory::create();
 
@@ -30,16 +55,44 @@
 		$regular_comments = get_comments();
 		$comments = array_merge($regular_comments, $trash_comments, $spam_comments);
 
+		$count = count( $users );
+		$progress = \WP_CLI\Utils\make_progress_bar( __( 'Obfuscating comments', 'gdpr' ), $count );
+
 		foreach ($comments as $comment) {
-			do_action( 'gdpr_sanitizer_update_comment', $comment->comment_ID, $faker );
 			$commentarr = $comment->to_array();
 			$commentarr['comment_author']       = $faker->name;
 			$commentarr['comment_author_email'] = $faker->safeEmail;
 			$commentarr['comment_author_url']   = $faker->url;
 			$commentarr['comment_author_IP']    = $faker->ipv4;
 			$commentarr['comment_agent']        = $faker->userAgent;
+			/**
+			 * Preupdate Comment.
+			 *
+			 * Triggered before a single comment is updated with fake information. Allows you to modify custom meta fields when the plugin is triggered.
+			 *
+			 * @since 1.0.0
+			 *
+			 * @param WP_Comment $comment original WP_Comment object.
+			 * @param array $commentarr new data about to be written to the database.
+			 * @param Faker $faker the faker object, made available for you to generate fake data for meta fields etc.
+			 */
+			do_action( 'gdpr_sanitizer_pre_update_comment', $comment, $commentarr, $faker );
 			wp_update_comment( $commentarr );
+			/**
+			 * Post update comment.
+			 *
+			 * Triggered after a single comment is updated with fake information. 
+			 *
+			 * @since 1.0.0
+			 *
+			 * @param WP_Comment $comment original WP_Comment object.
+			 * @param array $commentarr new data about to be written to the database.
+			 * @param Faker $faker the faker object, made available for you to generate fake data for meta fields etc.
+			 */
+			do_action( 'gdpr_sanitizer_post_update_comment', $comment, $commentarr, $faker );
+			$progress->tick();
 		}
+		$progress->finish();
 		WP_CLI::success( count( $comments ) . __( ' comments obfuscated.', 'gdpr-sanitizer' ) );
 	}
 	
@@ -63,31 +116,61 @@
 			WP_CLI::success( __( 'No users changed (did you exclude them all?)', 'gdpr-sanitizer' ) );
 			return;
 		}
+		$count = count( $users );
+		$progress = \WP_CLI\Utils\make_progress_bar( __( 'Obfuscating users', 'gdpr' ), $count );
 		foreach ( $users as $user ) {
 			$this->obfuscate_user( $user );
+			$progress->tick();
 		}
-		WP_CLI::success( count( $users ) . __( ' users obfuscated.', 'gdpr-sanitizer' ) );
+		$progress->finish();
+		WP_CLI::success( $count . __( ' users obfuscated.', 'gdpr-sanitizer' ) );
 	}
 
 	private function obfuscate_user( $user ) {
 		$faker = Faker\Factory::create();
+		$original_user = $user;
+		$new_data = [
+			'user_pass' => $faker->password,
+			'user_nicename' => $faker->name,
+			'user_email' => $faker->safeEmail,
+			'user_url' => $faker->url,
+			'display_name' => $faker->firstName,
+			'user_login' => $this->generate_unused_user_login(),
+		];
+		foreach ( $new_data as $key => $value ) {
+			$user->{$key} = $value;
+		}
 
-		do_action( 'gdpr_sanitizer_update_user', $user->ID, $user->user_email, $faker );
-
-		$user->data->user_pass = $faker->password;
-		$user->data->user_nicename = $faker->name;
-		$user->data->user_email = $faker->safeEmail;
-		$user->data->user_url = $faker->url;
-		$user->data->display_name = $faker->firstName;
-		
-		wp_update_user( $user );		
-		$this->update_user_login( $user->ID, $faker );
-		
+		/**
+		 * Pre update user.
+		 *
+		 * Triggered after a single user is updated with fake information. 
+		 *
+		 * @since 1.0.0
+		 *
+		 * @param WP_User $original_user original WP_User object.
+		 * @param WP_User $user new data about to be written to the database.
+		 * @param Faker $faker the faker object, made available for you to generate fake data for meta fields etc.
+		 */
+		do_action( 'gdpr_sanitizer_pre_update_user', $original_user, $user, $faker );
+		wp_update_user( $user );	
+		$this->update_user_login( $user->ID, $new_data['user_login'] );
+		/**
+		 * Post update user.
+		 *
+		 * Triggered after a single user is updated with fake information. 
+		 *
+		 * @since 1.0.0
+		 *
+		 * @param WP_User $original_user original WP_User object.
+		 * @param WP_User $user new data about to be written to the database.
+		 * @param Faker $faker the faker object, made available for you to generate fake data for meta fields etc.
+		 */
+		do_action( 'gdpr_sanitizer_pre_update_user', $original_user, $user, $faker );	
 	}
 
-	private function update_user_login( $user_id, $faker ) {
-		global $wpdb;
-
+	private function generate_unused_user_login() {
+		$faker = Faker\Factory::create();
 		// ensure the new fake username isn't already in use, keep generating them until we find one that isn't.
 		$new_user_login = false;
 		$sanity_check = 0;		
@@ -109,7 +192,12 @@
 				WP_CLI::error( __( 'Unable to find a fake username that was not already in use', 'gdpr-sanitizer' ) );
 			}
 		}
-		$wpdb->update( $wpdb->users, [ 'user_login' => $new_user_login, ], [ 'ID' => $user_id, ] );
+		return $new_user_login;
+	}
+
+	private function update_user_login( $user_id, $new_login ) {
+		global $wpdb;
+		$wpdb->update( $wpdb->users, [ 'user_login' => $new_login, ], [ 'ID' => $user_id, ] );
 	}
 
 	private function set_excluded_user_ids( $arg_string ) {
